58f91740111eeb16a1e0f6b3c35d28bd65deb2b9
kumarmanojgit <krmanoj72099@gmail.com>
2024-05-02 13:02:16 +0530

    add module css and component

--- cal/src/App.jsx
+++ cal/src/App.jsx
@@ -1,12 +1,20 @@
 
-import './App.css'
+import styles from "./App.module.css"
 
 function App() {
  
 
   return (
     <>
-      <h1>Hello world</h1>
+      <div className={styles.calculator}>
+      <input className={styles.display} type="text" />
+      <div className={styles.buttonsContainer}>
+        <button className={styles.button}>1</button>
+        <button className={styles.button}>2</button>
+        <button className={styles.button}>3</button>
+        <button className={styles.button}>+</button>
+      </div>
+      </div>
     </>
   )
 }
